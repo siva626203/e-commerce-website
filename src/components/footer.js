42a7f2d37fd747d155a057c2996a9deae439a310
siva626203 <sivakumar626203@gmail.com>
2022-03-11 21:53:51 +0530

    icon-attached

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import {Navbar,Nav,Container} from 'react-bootstrap'
+import { SiGmail } from "react-icons/si";
+import { AiFillYoutube } from "react-icons/ai";
+import { FaFacebookF } from 'react-icons/fa';
+
  class  Footer extends Component{
      render()
      {
          return(
             <Navbar bg="primary" variant="dark">
             <Container>
-            <Navbar.Brand>Contact</Navbar.Brand>
+            <Navbar.Brand>Contact Us</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">skcit@gmail.com</Nav.Link>
-              <Nav.Link href="#features">Youtube</Nav.Link>
-              <Nav.Link href="#pricing">Facebook</Nav.Link>
+              <div className="align-content-end">
+              <Nav.Link href="mailto:skcit@gmail.com"><SiGmail className='icon'/>Gmail</Nav.Link>
+              <Nav.Link href="https://www.youtube.com/c/SriKaliswariCollegeSivakasi"><AiFillYoutube className="icon"/>Youtube</Nav.Link>
+              <Nav.Link href="https://www.facebook.com/SriKaliswariCollegeAutonomous/"><FaFacebookF className="icon"/>Facebook</Nav.Link>
+              </div>
             </Nav>
             </Container>
           </Navbar>
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,45 @@
 import {Link} from 'react-router-dom';
 import {Navbar,Container,Nav,NavDropdown} from 'react-bootstrap';
 import React, {Component} from 'react';
-
+import '../App.css';
+import { AiFillHome} from "react-icons/ai";
+import { IoIosPeople } from "react-icons/io";
+import {RiGalleryFill} from 'react-icons/ri';
+import { MdOutlineSchool } from 'react-icons/md';
+import { GiAchievement } from 'react-icons/gi';
+import { BsBook } from 'react-icons/bs';
+import { MdOutlineLogin } from 'react-icons/md';
+import { ImEnter } from 'react-icons/im';
 class  Header extends Component{
      render()
      {
          return(
-
-            <div class="header">
+            <div className="header">
                 <h1>DEPARTMENT OF INFORMATION TECHNOLOGY</h1>
                 <Navbar bg="dark" variant="dark" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">Welcome</Navbar.Brand>
+    <Navbar.Brand href="#home" className="brand">Welcome</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
           
-        <Nav.Link as={Link} to="/e-commerce-website">Home</Nav.Link>
-        <Nav.Link as={Link} to="/faculty">Faculty</Nav.Link>
-        <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
-        <Nav.Link as={Link} to="/department">Department Activities</Nav.Link>
-        <NavDropdown title="Achievements" id="basic-nav-dropdown">
-          <NavDropdown.Item as={Link} to="/staff">Staff Achievement</NavDropdown.Item>
-          <NavDropdown.Item as={Link} to="/student">Students Achievement</NavDropdown.Item>
-          <Nav.Link as={Link} to="/e-commerce-website">About</Nav.Link>
+        <Nav.Link as={Link} to="/e-commerce-website" className="dropdown"><AiFillHome className='icon'/>Home</Nav.Link>
+        <Nav.Link as={Link} to="/faculty" className="dropdown"><IoIosPeople className='icon-m'/>Faculty</Nav.Link>
+        <Nav.Link as={Link} to="/gallery" className="dropdown"><RiGalleryFill className='icon-m'/>Gallery</Nav.Link>
+        <Nav.Link as={Link} to="/department" className="dropdown"><MdOutlineSchool className="icon-m"/>Department Activities</Nav.Link>
+        
+        <NavDropdown title="Achievements" id="basic-nav-dropdown" className="dropdown">
+          <NavDropdown.Item as={Link} to="/staff" className="dropdown-a"><GiAchievement className='icon'/>Staff Achievement</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/student" className="dropdown-a"><GiAchievement className='icon'/>Students Achievement</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link as={Link} to="/register">Register</Nav.Link>
-        <Nav.Link as={Link} to="/login">Login</Nav.Link> 
-        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+        <Nav.Link as={Link} to="/about" className="dropdown"><BsBook className='icon-m'/>About</Nav.Link>
+        <Nav.Link as={Link} to="/register" className="dropdown"><ImEnter className='icon-m'/>Register</Nav.Link>
+        <Nav.Link as={Link} to="/login" className="dropdown"><MdOutlineLogin className='icon-m'/>Login</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
-
-                </div>
-
-                
-            
+      </div>
          );
      }
 
